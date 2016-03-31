6d1d679491c6f8a581aeea6c0c6d3dc98027a24a
maximumayo <maxgmayo@gmail.com>
2016-03-30 17:07:29 -0700

    moved files

---
+++ practice21/answer.js
@@ -0,0 +1,12 @@
+answer.js
+
+
+
+function bipolar(){
+
+    for (var i = 0, j = 0; i <= 100, j >= -100; i++, j--){
+        console.log('i is: ' + i + ' and ' + 'j is: ' + j);
+    }
+}
+
+bipolar();
